51145ad227297e50574796a4b23a2e2a68287a1b
AmndineBlt <kookiechu@gmail.com>
2025-01-12 18:25:29 +0100

    mise à jour

--- js/timeline.js
+++ js/timeline.js
@@ -1,24 +1,32 @@
-//-------------------------------Script Timeline-------------------------------//
-// Sélectionne les éléments de la timeline
-const timelineItems = document.querySelectorAll(".timeline li");
+// timeline.js
 
-// Vérifie si un élément est visible dans la fenêtre
-function isElementInViewport(el) {
-  const rect = el.getBoundingClientRect();
-  return rect.top >= 0 && rect.bottom <= (window.innerHeight || document.documentElement.clientHeight);
-}
+export const timeline = {
+  // Sélectionner les éléments de la timeline
+  timelineItems: document.querySelectorAll(".timeline li"),
+  
+  isItemInView(item) {
+    console.log(item);
+    
+    let rect = item.getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  },
+  
+  callbackFunc() {
+    console.log(timeline.timelineItems);
+    
+    timeline.timelineItems.forEach(element => {
+      if (timeline.isItemInView(timeline.timelineItems[i])) {
+        timeline.timelineItems[i].setAttribute('data-aos', "fade-right");
+      }
+    });
+  },
+  
+};
 
-// Ajoute la classe "show" aux éléments visibles
-function revealTimelineItems() {
-  timelineItems.forEach((item) => {
-    if (isElementInViewport(item)) {
-      item.classList.add("show");
-    }
-  });
-}
-
-// Écoute les événements
-["load", "resize", "scroll"].forEach((event) =>
-  window.addEventListener(event, revealTimelineItems)
-);
-
+console.log(timeline.timelineItems);
